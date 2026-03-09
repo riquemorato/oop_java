4be809c4b5abf9fd76726e9dbaa45e3fd71a6915
riquemorato <henrique.morato@outlook.com>
2026-03-09 17:42:11 -0300

    feat: Implementação de array de objetos e chamada em loop for para printar na tela

--- lista_exercicios/src/main/java/lista_1/ex1/Main.java
+++ lista_exercicios/src/main/java/lista_1/ex1/Main.java
@@ -6,32 +6,21 @@ package lista_1.ex1;
 public class Main {
     public static void main(String[] args) {
 
+        Pessoa pessoas[] = new Pessoa[5];
         //Instanciando classe pessoa
-        Pessoa pessoa1 = new Pessoa("Henrique","Masculino", 31, false);
-        Pessoa pessoa2 = new Pessoa("Beatriz", "Feminino", 29, true);
-        Pessoa pessoa3 = new Pessoa("Valentina", "Feminino", 12, false);
-        Pessoa pessoa4 = new Pessoa("Enzo", "Masculino", 11, false);
-        Pessoa pessoa5 = new Pessoa("Alice", "Feminino", 2, false);
+        pessoas[0] = new Pessoa("Henrique","Masculino", 31, false);
+        pessoas[1] = new Pessoa("Beatriz", "Feminino", 29, true);
+        pessoas[2] = new Pessoa("Valentina", "Feminino", 12, false);
+        pessoas[3] = new Pessoa("Enzo", "Masculino", 11, false);
+        pessoas[4] = new Pessoa("Alice", "Feminino", 2, false);
 
         //instanciando classe Churrasco
         Churrasco churras = new Churrasco();
 
         //imprimindo
-        System.out.println(pessoa1.toString());
-        churras.verificarConsumo(pessoa1);
-
-        System.out.println(pessoa2.toString());
-        churras.verificarConsumo(pessoa2);
-
-        System.out.println(pessoa3.toString());
-        churras.verificarConsumo(pessoa3);
-
-        System.out.println(pessoa4.toString());
-        churras.verificarConsumo(pessoa4);
-
-        System.out.println(pessoa5.toString());
-        churras.verificarConsumo(pessoa5);
-
-
+        for(int i = 0; i < 5; i++) {
+            System.out.println(pessoas[i].toString());
+            churras.verificarConsumo(pessoas[i]);
+        }
     }
 }
